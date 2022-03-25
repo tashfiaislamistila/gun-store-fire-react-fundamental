a77b37e0f7d091242538234ada7fbe4a50b15835
tashfiaislamistila <tashfia.cse@gmail.com>
2022-03-24 21:04:48 -0700

    show single data in UI commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import Card from './Component/Card/Card';
 import Navbar from './Component/Navbar/Navbar';
 function App() {
   const [guns, setGuns] = useState([]);
-  console.log(guns);
+  // console.log(guns);
   useEffect(() => {
     fetch('data.json')
       .then(res => res.json())
       .then(data => setGuns(data))
 
   }, []);
+
   return (
-    <div className="App">
-      <Navbar></Navbar>
+    <div>
+      <Navbar />
+      <Card />
+      {
+        guns.map((gun) => (
+          <h1 key={gun.id}>{gun.name}</h1>))
+      }
     </div>
   );
 }
