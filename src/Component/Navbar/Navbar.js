d04ed4641cf22a95912717fa7da076a309670656
tashfiaislamistila <tashfia.cse@gmail.com>
2022-03-24 20:39:45 -0700

    load data navbar commit

---
+++ src/Component/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navbar.css'
+const Navbar = () => {
+    return (
+        <div>
+            <nav className='navbar'>
+                <h1>Gun Fire Store</h1>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
